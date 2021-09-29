6fd8f308f36e2851e480919cb108622f0f2fef1b
zpalawit-palasak <gravity.moons@gmail.com>
2021-09-29 22:40:56 +0700

    update DAG file

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -2,12 +2,28 @@ from airflow.models import DAG
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
+from pandas import json_normalize
 import json
 
 default_args = {
     "start_date": datetime(2021, 1, 10)
 }
+
+def _processing_user(ti):
+    users = ti.xcom_pull(task_ids=["extracting_user"])
+    if not len(users) or "results" not in users[0]:
+        raise ValueError("User is empty.")
+    user = users[0]["results"][0]
+    processed_user = json_normalize({
+        "username": user["login"]["username"],
+        "password": user["login"]["password"],
+        "fname": user["name"]["first"],
+        "lname": user["name"]["last"]
+    })
+    processed_user.to_csv("/opt/airflow/dbs/processing_user.csv", index=None, header=False)
+    
 
 with DAG(
     "user_processing",
@@ -43,5 +59,10 @@ with DAG(
         endpoint="api/",
         response_filter= lambda response: json.loads(response.text),
         log_response=True
+    )
+
+    python_task = PythonOperator(
+        task_id="processing_user",
+        python_callable=_processing_user
     )
      
