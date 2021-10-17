552f0b060a8e483849e539d4777676ec29750638
zpalawit-palasak <gravity.moons@gmail.com>
2021-10-17 20:32:12 +0700

    Add XCOM

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -2,7 +2,8 @@ from airflow.models import DAG
 from datetime import datetime
 from airflow.operators.bash import BashOperator
 from airflow.operators.subdag import SubDagOperator
-from subDAGs.subdag_parallel_dag import subdag_parallel_dag
+# from subDAGs.subdag_parallel_dag import subdag_parallel_dag
+from airflow.utils.task_group import TaskGroup
 
 default_args = {
     "start_date": datetime(2021, 10, 3)
@@ -13,7 +14,7 @@ with DAG(
     default_args=default_args,
     description="A simple parallel DAG",
     schedule_interval="@daily",
-    tags=["example"],
+    tags=["myDAG"],
     catchup=False
 ) as dag:
 
@@ -22,14 +23,33 @@ with DAG(
         bash_command="sleep 3 && echo 'task-1 done.'"
     )
 
-    processing = SubDagOperator(
-        task_id="processing_tasks",
-        subdag=subdag_parallel_dag("parallel_dag", "processing_tasks", default_args)
-    )
+    with TaskGroup("processing_tasks") as processing_tasks:
+        
+        task2 = BashOperator(
+            task_id="task-2",
+            bash_command="sleep 3 && echo 'task-2 done.'"
+        )
+
+        with TaskGroup("spark_processing_group") as spark_processing_group:
+            task3 = BashOperator(
+            task_id="task-3",
+            bash_command="sleep 3 && echo 'task-3 done.'"
+        )
+
+        with TaskGroup("flink_processing_group") as flink_processing_group:
+            task3 = BashOperator(
+            task_id="task-3",
+            bash_command="sleep 3 && echo 'task-3 done.'"
+        )
+
+    # processing = SubDagOperator(
+    #     task_id="processing_tasks",
+    #     subdag=subdag_parallel_dag("parallel_dag", "processing_tasks", default_args)
+    # )
 
     task4 = BashOperator(
         task_id="task-4",
         bash_command="sleep 3 && echo 'task-4 done.'"
     )
 
-    task1 >> processing >> task4
+    task1 >> processing_tasks >> task4
